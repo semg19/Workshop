e6a0f38b7c6ec1611ef633444b94d456e84a092a
semg19 <0913247@hr.nl>
2018-10-16 14:54:39 +0200

    first commit

---
+++ app/src/main/java/workshop/com/sem/workshop_oefening/MainActivity.java
@@ -0,0 +1,16 @@
+package workshop.com.sem.workshop_oefening;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Workshop");
+    }
+}
